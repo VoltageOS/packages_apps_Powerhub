825dc41473265cdaa15becd7fa2440b1721ddae0
Ido Ben-Hur <idoybh2@gmail.com>
2020-05-19 13:34:09 +0300

    Powerhub: ColorPicker: Enable the reset button when setting a default at runtime
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
+++ src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
@@ -272,6 +272,8 @@ public class ColorPickerPreference extends Preference implements
 
     public void setDefaultColor(int color) {
         mDefValue = color;
+        mUsesDefaultButton = true;
+        setDefaultButton();
     }
 
     /**
