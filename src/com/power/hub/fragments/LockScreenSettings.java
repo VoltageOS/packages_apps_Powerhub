bdd3b44324eb68699f23c56a158123c4e87a2945
idoybh <idoybh@users.noreply.github.com>
2021-12-11 00:13:54 +0000

    Powerhub: Allow scheduling always on display [2/2] From sunset to sunrise or at a custom time
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/power/hub/fragments/LockScreenSettings.java
+++ src/com/power/hub/fragments/LockScreenSettings.java
@@ -61,10 +61,13 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
 			
     private static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
     private static final String FINGERPRINT_ERROR_VIB = "fingerprint_error_vib";
+    private static final String AOD_SCHEDULE_KEY = "always_on_display_schedule";
 
     private FingerprintManager mFingerprintManager;
     private SwitchPreference mFingerprintSuccessVib;
     private SwitchPreference mFingerprintErrorVib;
+			
+    Preference mAODPref;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -96,11 +99,32 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
             prefSet.removePreference(mFingerprintSuccessVib);
             prefSet.removePreference(mFingerprintErrorVib);
         }
+		
+	mAODPref = findPreference(AOD_SCHEDULE_KEY);
+        updateAlwaysOnSummary();
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        updateAlwaysOnSummary();
+    }
+
+    private void updateAlwaysOnSummary() {
+        if (mAODPref == null) return;
+        int mode = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
+                Settings.Secure.DOZE_ALWAYS_ON_AUTO_MODE, 0, UserHandle.USER_CURRENT);
+        switch (mode) {
+            case 0:
+                mAODPref.setSummary(R.string.disabled);
+                break;
+            case 1:
+                mAODPref.setSummary(R.string.night_display_auto_mode_twilight);
+                break;
+            case 2:
+                mAODPref.setSummary(R.string.night_display_auto_mode_custom);
+                break;
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
