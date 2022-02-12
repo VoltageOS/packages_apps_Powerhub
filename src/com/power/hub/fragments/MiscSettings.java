3cba41886929e0dccefe41118b44bc20f07fd5cc
Pranav Vashi <neobuddy89@gmail.com>
2022-02-12 22:55:13 +0530

    Powerhub: User toggle for GamesProp [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/power/hub/fragments/MiscSettings.java
+++ src/com/power/hub/fragments/MiscSettings.java
@@ -47,8 +47,12 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
-    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
+    private static final String KEY_GAMES_SPOOF = "use_games_spoof";
 
+    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
+    private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
+
+    private SwitchPreference mGamesSpoof;
     private SwitchPreference mPhotosSpoof;
 
     @Override
@@ -60,6 +64,10 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
+
+        mGamesSpoof = (SwitchPreference) findPreference(KEY_GAMES_SPOOF);
+        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
+        mGamesSpoof.setOnPreferenceChangeListener(this);
 
         mPhotosSpoof = (SwitchPreference) findPreference(KEY_PHOTOS_SPOOF);
         mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
@@ -79,7 +87,11 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mPhotosSpoof) {
+        if (preference == mGamesSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            return true;
+        } else if (preference == mPhotosSpoof) {
             SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
             return true;
         }
