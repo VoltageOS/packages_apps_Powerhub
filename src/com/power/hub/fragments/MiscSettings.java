db8a3b22b8817c04539062341e45a6bff59af2f5
Shripal17 <shripal17@gmail.com>
2022-04-02 23:21:22 +0300

    Powerhub:[SQUASH] Add Flashlight blink on incoming calls
    Add DND & Rate settings to flash on call [3/3]
    
    Allow to flash on call only when entirely silent [3/3]
    
    Add Flashlight blink on incoming calls [3/3]
    
    Change-Id: I868ad06f7b41234c03a0157ee6c57e5a9dd7c8a2
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/power/hub/fragments/MiscSettings.java
+++ src/com/power/hub/fragments/MiscSettings.java
@@ -1,6 +1,7 @@
 package com.power.hub.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.voltage.VoltageUtils;
 
 import android.os.Bundle;
 import android.content.Intent;
@@ -17,6 +18,7 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
+import androidx.preference.PreferenceCategory;
 import com.power.hub.preferences.CustomSeekBarPreference;
 import android.provider.Settings;
 import android.os.SystemProperties;
@@ -35,6 +37,7 @@ import java.util.HashSet;
 
 import android.content.pm.PackageManager.NameNotFoundException;
 import com.android.settings.SettingsPreferenceFragment;
+import com.power.hub.preferences.CustomSeekBarPreference;
 import com.power.hub.preferences.SystemSettingMasterSwitchPreference;
 import com.power.hub.preferences.SystemSettingListPreference;
 import com.power.hub.preferences.SecureSettingSwitchPreference;
@@ -50,6 +53,15 @@ import com.power.hub.preferences.SystemSettingSwitchPreference;
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class MiscSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_FLASH_ON_CALL = "flashlight_on_call";
+    private static final String PREF_FLASH_ON_CALL_DND = "flashlight_on_call_ignore_dnd";
+    private static final String PREF_FLASH_ON_CALL_RATE = "flashlight_on_call_rate";
+    private static final String FLASHLIGHT_CATEGORY = "flashlight_category";
+
+    private CustomSeekBarPreference mFlashOnCallRate;
+    private SystemSettingSwitchPreference mFlashOnCallIgnoreDND;
+    private SystemSettingListPreference mFlashOnCall;
 
     private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
     private static final String KEY_GAMES_SPOOF = "use_games_spoof";
@@ -75,6 +87,8 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
         final PackageManager mPm = getActivity().getPackageManager();
+        final Context mContext = getActivity().getApplicationContext();
+        final Resources res = mContext.getResources();
 
         mGamesSpoof = (SwitchPreference) findPreference(KEY_GAMES_SPOOF);
         mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
@@ -83,16 +97,35 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         mPhotosSpoof = (SwitchPreference) findPreference(KEY_PHOTOS_SPOOF);
         mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
         mPhotosSpoof.setOnPreferenceChangeListener(this);
-		
-		Resources res = null;
+
         Context ctx = getContext();
         float density = Resources.getSystem().getDisplayMetrics().density;
 
-        try {
-            res = ctx.getPackageManager().getResourcesForApplication("com.android.systemui");
-        } catch (NameNotFoundException e) {
-            e.printStackTrace();
+        if (!VoltageUtils.deviceHasFlashlight(mContext)) {
+            final PreferenceCategory flashlightCategory =
+                    (PreferenceCategory) findPreference(FLASHLIGHT_CATEGORY);
+            prefSet.removePreference(flashlightCategory);
+        } else {
+            mFlashOnCallRate = (CustomSeekBarPreference)
+                    findPreference(PREF_FLASH_ON_CALL_RATE);
+            int value = Settings.System.getInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, 1);
+            mFlashOnCallRate.setValue(value);
+            mFlashOnCallRate.setOnPreferenceChangeListener(this);
+
+            mFlashOnCallIgnoreDND = (SystemSettingSwitchPreference)
+                    findPreference(PREF_FLASH_ON_CALL_DND);
+            value = Settings.System.getInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, 0);
+            mFlashOnCallIgnoreDND.setVisible(value > 1);
+            mFlashOnCallRate.setVisible(value != 0);
+
+            mFlashOnCall = (SystemSettingListPreference)
+                    findPreference(PREF_FLASH_ON_CALL);
+            mFlashOnCall.setSummary(mFlashOnCall.getEntries()[value]);
+            mFlashOnCall.setOnPreferenceChangeListener(this);
         }
+
         mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
         mFingerprintSuccessVib = findPreference(FINGERPRINT_SUCCESS_VIB);
         mFingerprintErrorVib = findPreference(FINGERPRINT_ERROR_VIB);
@@ -118,6 +151,7 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mGamesSpoof) {
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
@@ -137,9 +171,23 @@ public class MiscSettings extends SettingsPreferenceFragment implements
                     Settings.System.FP_ERROR_VIBRATE, value ? 1 : 0);
             return true;
         }
+        if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, value);
+            mFlashOnCall.setSummary(mFlashOnCall.getEntries()[value]);
+            mFlashOnCallIgnoreDND.setVisible(value > 1);
+            mFlashOnCallRate.setVisible(value != 0);
+            return true;
+        } else if (preference == mFlashOnCallRate) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, value);
+            return true;
+        } else {
         return false;
     }
-	
+  }
 
     @Override
     public int getMetricsCategory() {
