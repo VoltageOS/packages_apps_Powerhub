dfad558cf188ff190738b48aa0e8163214bb828d
spkal01 <kalligeross@gmail.com>
2022-08-26 09:53:06 +0300

    Powerhub: CLock Customization
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/power/hub/fragments/StatusBarSettings.java
+++ src/com/power/hub/fragments/StatusBarSettings.java
@@ -8,7 +8,6 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.res.Resources;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -20,23 +19,22 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
-
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
 import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
-
-import com.android.settings.SettingsPreferenceFragment;
-import com.power.hub.preferences.CustomSeekBarPreference;
-import com.power.hub.preferences.SystemSettingSeekBarPreference;
-import com.power.hub.preferences.SystemSettingListPreference;
 import com.power.hub.preferences.SystemSettingSwitchPreference;
-import com.power.hub.preferences.SystemSettingMasterSwitchPreference;
+import com.power.hub.preferences.SystemSettingSeekBarPreference;
+import com.power.hub.preferences.SecureSettingSwitchPreference;
+import com.power.hub.preferences.SystemSettingListPreference;
+import com.android.settings.SettingsPreferenceFragment;
+import com.power.hub.preferences.SystemSettingSeekBarPreference;
 import com.android.settings.Utils;
-import com.android.internal.util.voltage.VoltageUtils;
 import android.util.Log;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settingslib.search.SearchIndexable;
-import android.provider.SearchIndexableResource;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -44,54 +42,41 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
 
-@SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
+
+    private SystemSettingListPreference mStatusBarClock;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.powerhub_statusbar);
-		
-		ContentResolver resolver = getActivity().getContentResolver();
 
         PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mStatusBarClock =
+                (SystemSettingListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
+
+        // Adjust status bar preferences for RTL
+        if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+            mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_rtl);
+            mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_rtl);
+        }
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
         return false;
     }
-	
+
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.VOLTAGE;
     }
-	
-	/**
-     * For Search.
-     */
-
-    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
-
-                @Override
-                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                        boolean enabled) {
-                    ArrayList<SearchIndexableResource> result =
-                            new ArrayList<SearchIndexableResource>();
-                    SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.powerhub_statusbar;
-                    result.add(sir);
-                    return result;
-                }
-
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    List<String> keys = super.getNonIndexableKeys(context);
-                    return keys;
-                }
-    };
 
 }
