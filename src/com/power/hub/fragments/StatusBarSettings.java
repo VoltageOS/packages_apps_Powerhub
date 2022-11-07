42d81c72bea654671734a56bf79396ef66e825c3
tejasvp25 <tejasvp25@gmail.com>
2022-11-07 22:39:21 +0530

    Powerhub: Move battery styles to seperate fragment
    Signed-off-by: tejasvp25 <tejasvp25@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/power/hub/fragments/StatusBarSettings.java
+++ src/com/power/hub/fragments/StatusBarSettings.java
@@ -43,87 +43,40 @@ import java.util.HashMap;
 import java.util.Collections;
 
 public class StatusBarSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+                OnPreferenceChangeListener {
 
-    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
+        private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
 
-    private SystemSettingListPreference mStatusBarClock;
+        private SystemSettingListPreference mStatusBarClock;
 
-    private static final String KEY_STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-    private static final String KEY_STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
-    private static final String KEY_STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
+        @Override
+        public void onCreate(Bundle icicle) {
+                super.onCreate(icicle);
 
-    private static final int BATTERY_STYLE_PORTRAIT = 0;
-    private static final int BATTERY_STYLE_TEXT = 4;
-    private static final int BATTERY_STYLE_HIDDEN = 5;
+                addPreferencesFromResource(R.xml.powerhub_statusbar);
 
-    private SystemSettingListPreference mBatteryPercent;
-    private SystemSettingListPreference mBatteryStyle;
-    private SwitchPreference mBatteryTextCharging;
+                PreferenceScreen prefSet = getPreferenceScreen();
+                ContentResolver resolver = getActivity().getContentResolver();
 
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+                mStatusBarClock = (SystemSettingListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
 
-        addPreferencesFromResource(R.xml.powerhub_statusbar);
+                // Adjust status bar preferences for RTL
+                if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+                        mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_rtl);
+                        mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_rtl);
+                }
 
-        PreferenceScreen prefSet = getPreferenceScreen();
-        ContentResolver resolver = getActivity().getContentResolver();
-
-        mStatusBarClock =
-                (SystemSettingListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
-
-        // Adjust status bar preferences for RTL
-        if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
-            mStatusBarClock.setEntries(R.array.status_bar_clock_position_entries_rtl);
-            mStatusBarClock.setEntryValues(R.array.status_bar_clock_position_values_rtl);
         }
 
-        int batterystyle = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
-        int batterypercent = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
-
-        mBatteryStyle = (SystemSettingListPreference) findPreference(KEY_STATUS_BAR_BATTERY_STYLE);
-        mBatteryStyle.setOnPreferenceChangeListener(this);
-
-        mBatteryPercent = (SystemSettingListPreference) findPreference(KEY_STATUS_BAR_SHOW_BATTERY_PERCENT);
-        mBatteryPercent.setEnabled(
-                batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
-        mBatteryPercent.setOnPreferenceChangeListener(this);
-
-        mBatteryTextCharging = (SwitchPreference) findPreference(KEY_STATUS_BAR_BATTERY_TEXT_CHARGING);
-        mBatteryTextCharging.setEnabled(batterystyle == BATTERY_STYLE_HIDDEN ||
-                (batterystyle != BATTERY_STYLE_TEXT && batterypercent != 2));
-
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mBatteryStyle) {
-            int value = Integer.parseInt((String) newValue);
-            int batterypercent = Settings.System.getIntForUser(getContentResolver(),
-                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
-            mBatteryPercent.setEnabled(
-                    value != BATTERY_STYLE_TEXT && value != BATTERY_STYLE_HIDDEN);
-            mBatteryTextCharging.setEnabled(value == BATTERY_STYLE_HIDDEN ||
-                    (value != BATTERY_STYLE_TEXT && batterypercent != 2));
-            return true;
-        } else if (preference == mBatteryPercent) {
-            int value = Integer.parseInt((String) newValue);
-            int batterystyle = Settings.System.getIntForUser(getContentResolver(),
-                    Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
-            mBatteryTextCharging.setEnabled(batterystyle == BATTERY_STYLE_HIDDEN ||
-                    (batterystyle != BATTERY_STYLE_TEXT && value != 2));
-            return true;
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+                // ContentResolver resolver = getActivity().getContentResolver();
+                return false;
         }
-        return false;
-    }
 
-    @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.VOLTAGE;
-    }
+        @Override
+        public int getMetricsCategory() {
+                return MetricsProto.MetricsEvent.VOLTAGE;
+        }
 
 }
