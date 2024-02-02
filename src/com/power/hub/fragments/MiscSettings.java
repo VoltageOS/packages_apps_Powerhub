f1054eb2676605ed731133f7cb8fe0580f6d013f
Joey Huab <joey@evolution-x.org>
2024-02-02 20:07:20 +0000

    Powerhub: move spoofing in own fragment
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/power/hub/fragments/MiscSettings.java
+++ src/com/power/hub/fragments/MiscSettings.java
@@ -18,8 +18,6 @@ import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 import com.power.hub.preferences.CustomSeekBarPreference;
 import android.provider.Settings;
-import android.os.SystemProperties;
-import android.widget.Toast;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import java.util.Locale;
@@ -48,16 +46,7 @@ import java.util.List;
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
-    private static final String KEY_GAMES_SPOOF = "use_games_spoof";
-
-    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
-    private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
-
     private static final String SMART_PIXELS = "smart_pixels";
-
-    private SwitchPreference mGamesSpoof;
-    private SwitchPreference mPhotosSpoof;
 
     private Preference mSmartPixels;
 
@@ -68,16 +57,7 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.powerhub_misc);
 
         final PreferenceScreen prefSet = getPreferenceScreen();
-        final ContentResolver resolver = getActivity().getContentResolver();
 
-        mGamesSpoof = (SwitchPreference) findPreference(KEY_GAMES_SPOOF);
-        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
-        mGamesSpoof.setOnPreferenceChangeListener(this);
-
-        mPhotosSpoof = (SwitchPreference) findPreference(KEY_PHOTOS_SPOOF);
-        mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
-        mPhotosSpoof.setOnPreferenceChangeListener(this);
-		
            mSmartPixels = (Preference) findPreference(SMART_PIXELS);
            boolean mSmartPixelsSupported = getResources().getBoolean(
                  com.android.internal.R.bool.config_supportSmartPixels);
@@ -88,24 +68,14 @@ public class MiscSettings extends SettingsPreferenceFragment implements
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mGamesSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
-            return true;
-        } else if (preference == mPhotosSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
-            return true;
-        }
         return false;
     }
-	
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.VOLTAGE;
     }
-	
+
 	/**
      * For Search.
      */
