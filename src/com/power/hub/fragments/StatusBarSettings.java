939a2221fead50135bd49ebe0ac864f2979b082d
Dmitrii <bankersenator@gmail.com>
2025-07-26 18:38:23 +0000

    Powerhub: modern icons switch 2/2
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/power/hub/fragments/StatusBarSettings.java
+++ src/com/power/hub/fragments/StatusBarSettings.java
@@ -54,6 +54,10 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 
     private SystemSettingListPreference mStatusBarClock;
 
+    private static final String PREF_NEW_STATUS_BAR_ICONS = "new_status_bar_icons";
+
+    private SwitchPreferenceCompat mNewStatusBarIconsPref;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -62,12 +66,57 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 		ContentResolver resolver = getActivity().getContentResolver();
 
         PreferenceScreen prefSet = getPreferenceScreen();
+
+        mNewStatusBarIconsPref = findPreference(PREF_NEW_STATUS_BAR_ICONS);
+        mNewStatusBarIconsPref.setOnPreferenceChangeListener(this);
+
+        boolean newIconsEnabled = Settings.System.getIntForUser(
+            resolver, "new_status_bar_icons_enabled", 0, UserHandle.USER_CURRENT) == 1;
+        mNewStatusBarIconsPref.setChecked(newIconsEnabled);
+        updatePreferenceStates(newIconsEnabled);
+    }
+
+    /**
+     * This is the helper method that enables or disables all incompatible preferences.
+     * @param newIconsEnabled true if the new UI is on, which means old options should be disabled.
+     */
+    private void updatePreferenceStates(boolean newIconsEnabled) {
+        // We use !newIconsEnabled because if the new UI is ON (true),
+        // the old preferences should be DISABLED (false).
+        final boolean oldPrefsEnabled = !newIconsEnabled;
+
+        findPreference("systemui_tuner_statusbar").setEnabled(oldPrefsEnabled);
+        findPreference("clock").setEnabled(oldPrefsEnabled);
+        findPreference("battery_bar_category").setEnabled(oldPrefsEnabled);
+        //findPreference("network_traffic_settings").setEnabled(oldPrefsEnabled);
+        //findPreference("ongoing_progress_settings").setEnabled(oldPrefsEnabled);
+        findPreference("show_fourg_icon").setEnabled(oldPrefsEnabled);
+        findPreference("data_disabled_icon").setEnabled(oldPrefsEnabled);
+        findPreference("enable_camera_privacy_indicator").setEnabled(oldPrefsEnabled);
+        findPreference("enable_location_privacy_indicator").setEnabled(oldPrefsEnabled);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         ContentResolver resolver = getActivity().getContentResolver();
+        final Context context = getContext();
+
+        if (preference == mNewStatusBarIconsPref) {
+            boolean value = (Boolean) objValue;
+
+            updatePreferenceStates(value);
+
+            Settings.System.putIntForUser(resolver, "status_bar_root_modernization_enabled",
+                    value ? 1 : 0, UserHandle.USER_CURRENT);
+            Settings.System.putIntForUser(resolver, "new_status_bar_icons_enabled",
+                    value ? 1 : 0, UserHandle.USER_CURRENT);
+
+            VoltageUtils.showSystemUiRestartDialog(context);
+            return true;
+        }
+
         return false;
+
     }
 
     @Override
